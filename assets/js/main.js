0a1f0e73bef6880b4a94d6668f56022b2a5b18e5
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2024-04-21 15:01:38 -0300

    details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,74 @@
-const card = document.querySelector('#card');
-const submit = document.querySelector('#sub');
-const bandeira = document.querySelectorAll('.bandeira');
-const nomebandeira = document.querySelector('#front > h1');
+const card = document.querySelector("#card");
+const submit = document.querySelector("#sub");
+const bandeira = document.querySelectorAll(".bandeira");
+const nomebandeira = document.querySelector("#front > h1");
 
-const inputNum = document.querySelector('#num');
-const inputVal = document.querySelector('#val');
-const inputCvv = document.querySelector('#cvv');
-const inputName = document.querySelector('#nome');
+const inputNum = document.querySelector("#num");
+const inputVal = document.querySelector("#val");
+const inputCvv = document.querySelector("#cvv");
+const inputName = document.querySelector("#nome");
 
-const cardnum = document.querySelector('#number');
-const cardval = document.querySelector('#front > small');
-const cardname = document.querySelector('#name');
-const cardcvv = document.querySelector('#back p');
+const cardnum = document.querySelector("#number");
+const cardval = document.querySelector("#front > small");
+const cardname = document.querySelector("#name");
+const cardcvv = document.querySelector("#back p");
 
-card.addEventListener('click', () => {
-    card.classList.toggle('flip');
-})
+card.addEventListener("click", () => {
+    card.classList.toggle("flip");
+});
 
-inputNum.addEventListener('input', () => {
-    card.classList.add('flip');
+inputNum.addEventListener("input", () => {
+    card.classList.add("flip");
     cardnum.innerHTML = inputNum.value;
-    cardnum.innerHTML == '' ? cardnum.innerHTML = '•••• •••• •••• ••••' : undefined;
+    cardnum.innerHTML == "" ? (cardnum.innerHTML = "•••• •••• •••• ••••") : undefined;
     if (inputNum.value[0] == 4) {
-        nomebandeira.innerHTML = 'Visa';
+        nomebandeira.innerHTML = "Visa";
         bandeira.forEach((e) => {
-            e.src = 'assets/images/visa.png'
-        }) 
+            e.src = "assets/images/visa.png";
+        });
     } else {
-        nomebandeira.innerHTML = 'MasterCard';
+        nomebandeira.innerHTML = "MasterCard";
         bandeira.forEach((e) => {
-            e.src = 'assets/images/mastercard.png'
-        }) 
+            e.src = "assets/images/mastercard.png";
+        });
     }
     isFull();
-})
+});
 
-inputVal.addEventListener('input', () => {
-    card.classList.add('flip');
+inputVal.addEventListener("input", () => {
+    card.classList.add("flip");
     cardval.innerHTML = inputVal.value;
-    cardval.innerHTML == '' ? cardval.innerHTML = '••/••' : undefined;
+    cardval.innerHTML == "" ? (cardval.innerHTML = "••/••") : undefined;
     isFull();
-})
+});
 
-inputName.addEventListener('input', () => {
-    card.classList.add('flip');
+inputName.addEventListener("input", () => {
+    card.classList.add("flip");
     cardname.innerHTML = inputName.value;
-    cardname.innerHTML == '' ? cardname.innerHTML = 'Nome no cartão' : undefined;
+    cardname.innerHTML == "" ? (cardname.innerHTML = "Nome no cartão") : undefined;
     isFull();
-})
+});
 
-inputCvv.addEventListener('input', () => {
-    card.classList.remove('flip');
+inputCvv.addEventListener("input", () => {
+    card.classList.remove("flip");
     cardcvv.innerHTML = inputCvv.value;
-    cardcvv.innerHTML == '' ? cardcvv.innerHTML = '•••' : undefined;
+    cardcvv.innerHTML == "" ? (cardcvv.innerHTML = "•••") : undefined;
     isFull();
-})
+});
 
 submit.addEventListener("click", (e) => {
-    e.preventDefault()
-})
+    e.preventDefault();
+});
 
 function isFull() {
-    if (inputCvv.value.length == 3 && inputNum.value.length == (4*4+3) && inputVal.value.length == 5 && inputName.value) {
-        submit.style.display = 'block';
+    if (
+        inputCvv.value.length == 3 &&
+        inputNum.value.length == 4 * 4 + 3 &&
+        inputVal.value.length == 5 &&
+        inputName.value
+    ) {
+        submit.style.display = "block";
+    } else {
+        submit.style.display = "none";
     }
 }
